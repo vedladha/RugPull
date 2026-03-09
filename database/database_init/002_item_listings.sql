91dbce01e23e1b16b1110f8d5d44f112194556a7
JOSEF WOLF <wolf27@wisc.edu>
2026-03-09 16:11:24 -0500

    Changed database directory structure to make additions easier and added item listings table

---
+++ database/database_init/002_item_listings.sql
@@ -0,0 +1,23 @@
+SET NAMES 'utf8mb4';
+SET time_zone = '+00:00';
+
+START TRANSACTION;
+
+-- Items
+CREATE TABLE Items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    price DECIMAL(30, 8), -- Price in crypto amount
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    -- category_id INT, Will be added later with categories
+    stock INT DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT fk_items_users
+        FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+COMMIT;
