3043e95f2013bea21d2bc46129af49d564be645c
ewerts <ewerts@wisc.edu>
2026-03-10 19:49:29 -0500

    added items model, repo, table.

---
+++ backend/src/main/java/com/example/demo/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Item;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+    Optional<Item> findByItemIdAndDeletedFalse(Integer itemId);
+}
