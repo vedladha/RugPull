1058d7bd918681114675de402010bfbefdf330da
mholder2 <mholder2@wisc.edu>
2026-03-12 11:07:18 -0500

    Add wallet transaction API

--- rpc_currency/app/src/test/java/edu/wisc/t32/impl/WalletServiceImplTest.java
+++ rpc_currency/app/src/test/java/edu/wisc/t32/impl/WalletServiceImplTest.java
@@ -1,10 +1,13 @@
 package edu.wisc.t32.impl;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import edu.wisc.t32.AbstractCryptoTest;
+import edu.wisc.t32.api.TransferResponse;
 import edu.wisc.t32.api.Wallet;
 import edu.wisc.t32.api.WalletService;
 import org.junit.jupiter.api.AfterAll;
@@ -23,7 +26,7 @@ public class WalletServiceImplTest extends AbstractCryptoTest {
 
   @BeforeAll
   void setupResources() {
-    token = spinTestToken(10);
+    token = spinTestToken(1000_00);
     walletService = assertDoesNotThrow(
         () -> WalletService.getService(this.operatorId.toString(), this.operatorKey.toString(),
             token.tokenId().toString()),
@@ -45,13 +48,9 @@ public class WalletServiceImplTest extends AbstractCryptoTest {
     // this uses are wallet service method
     Wallet wallet = assertDoesNotThrow(() -> this.walletService.createWallet(1),
         "The wallet should be successfully created, but was not");
-    // below converts the accountId and privateKey into a ClientPair
-    AccountId accountId = AccountId.fromString(wallet.getWalletId());
-    PrivateKey key = PrivateKey.fromStringECDSA(wallet.getWalletPrivateKey());
-    // this object is used to manage and delete our test account created via the Wallet object
-    final ClientPair pair = new ClientPair(accountId, key, this.operatorId, this.client);
-    assertDoesNotThrow(pair::close,
-        "The created wallet should be successfully deleted, but was not");
+
+    // we will use our teardown method which automatically helps us destroy this account
+    teardownWallet(wallet);
   }
 
   @Test
@@ -69,11 +68,155 @@ public class WalletServiceImplTest extends AbstractCryptoTest {
             + "exception was thrown during the process");
 
     // now let's try to delete the re-init wallet
+    teardownWallet(wallet);
+  }
+
+  @Test
+  void testWalletValidTransaction() {
+    final Wallet sender = assertDoesNotThrow(() -> this.walletService.createWallet(100_00),
+        "assertion on account creation failed");
+    final Wallet receiver = assertDoesNotThrow(() -> this.walletService.createWallet(1_00),
+        "assertion on account creation failed");
+
+    final TransferResponse response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.0f),
+            "The expected successful transfer did not complete successfully as "
+                + "there was some network error");
+    assertEquals(TransferResponse.SUCCESS, response,
+        "The transfer failed when response, but SUCCESS was expected");
+
+    // teardown our wallet accounts used for this test
+    teardownWallet(sender);
+    teardownWallet(receiver);
+  }
+
+  @Test
+  void testWalletValidTransactionWithDecimals() {
+    final Wallet sender = assertDoesNotThrow(() -> this.walletService.createWallet(100_00),
+        "assertion on account creation failed");
+    final Wallet receiver = assertDoesNotThrow(() -> this.walletService.createWallet(1_00),
+        "assertion on account creation failed");
+
+    TransferResponse response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.1f),
+            "The expected successful transfer did not complete successfully as "
+                + "there was some network error");
+    assertEquals(TransferResponse.SUCCESS, response,
+        "The transfer failed when response, but SUCCESS was expected");
+
+    response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.11f),
+            "The expected successful transfer did not complete successfully as "
+                + "there was some network error");
+    assertEquals(TransferResponse.SUCCESS, response,
+        "The transfer failed when response, but SUCCESS was expected");
+
+
+    // teardown our wallet accounts used for this test
+    teardownWallet(sender);
+    teardownWallet(receiver);
+  }
+
+  @Test
+  void testWalletInvalidDestinationTransaction() {
+    final Wallet sender = assertDoesNotThrow(() -> this.walletService.createWallet(100_00),
+        "assertion on account creation failed");
+    // give some random account id and private key. Deosn't actually matter as we want it to fail
+    final Wallet receiver =
+        this.walletService.createWallet("0.0.9999999", PrivateKey.generateECDSA().toString());
+
+    TransferResponse response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.2f));
+    assertEquals(TransferResponse.UNKNOWN_ACCOUNT, response,
+        "The transfer execpted the UNKNOWN_ACCOUNT response");
+
+    // now let's try with a deleted account
+    final Wallet receiver2 =
+        this.walletService.createWallet("0.0.8187658", PrivateKey.generateECDSA().toString());
+    response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.2f));
+    assertEquals(TransferResponse.UNKNOWN_ACCOUNT, response,
+        "The transfer execpted the UNKNOWN_ACCOUNT response");
+
+    teardownWallet(sender);
+  }
+
+  @Test
+  void testWalletAccountNotAssociatedTransaction() {
+    final Wallet sender = assertDoesNotThrow(() -> this.walletService.createWallet(100_00));
+    try (final ClientPair clientPair = spinTestAccount(0)) {
+      final Wallet receiver = this.walletService.createWallet(clientPair.accountId().toString(),
+          clientPair.privateKey().toString());
+
+      TransferResponse response =
+          assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 10.2f));
+      assertEquals(TransferResponse.NOT_ACCEPTING_TOKEN, response,
+          "The transfer execpted the NOT_ACCEPTING_TOKEN response");
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    teardownWallet(sender);
+  }
+
+  @Test
+  void testWalletInsufficientBalance() {
+    final Wallet sender = assertDoesNotThrow(() -> this.walletService.createWallet(1_00),
+        "assertion on account creation failed");
+    final Wallet receiver = assertDoesNotThrow(() -> this.walletService.createWallet(1_00),
+        "assertion on account creation failed");
+
+    final TransferResponse response =
+        assertDoesNotThrow(() -> this.walletService.transferBalance(sender, receiver, 100.23f),
+            "transaction threw when it should not have");
+    assertEquals(TransferResponse.INSUFFICIENT_BALANCE, response,
+        "The response should be INSUFFICIENT_BALANCE, but was not");
+
+    teardownWallet(sender);
+    teardownWallet(receiver);
+  }
+
+  @Test
+  void testWalletTransferArgumentValidation() {
+    final Wallet sender =
+        this.walletService.createWallet("0.0.1111111", PrivateKey.generateECDSA().toString());
+    final Wallet receiver =
+        this.walletService.createWallet("0.0.9999999", PrivateKey.generateECDSA().toString());
+
+    // top level nullability and sameness
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(receiver, receiver, 10f),
+        "Should not be able to transfer currency between the same account");
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(null, receiver, 10f),
+        "Should not be able to send from null wallet");
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(sender, null, 10f),
+        "Should not be able to transfer to null wallet");
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(null, null, 10f),
+        "Null wallets are not allowed");
+
+    // number validation
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(sender, receiver, 0.0f),
+        "Should not be able to transfer 0 tokens");
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(sender, receiver, -1.0f),
+        "Should not be able to transfer negative tokens");
+    assertThrows(IllegalArgumentException.class,
+        () -> this.walletService.transferBalance(sender, receiver, 10.123f),
+        "Should not be able to transfer amount of more than 2 decimals");
+  }
+
+  private void teardownWallet(Wallet wallet) {
+    // now let's try to delete the re-init wallet
     AccountId accountId = AccountId.fromString(wallet.getWalletId());
     PrivateKey key = PrivateKey.fromStringECDSA(wallet.getWalletPrivateKey());
-
     // now we close like before by adding our data back into a ClientPair which is AutoClosable
-    final ClientPair pair = new ClientPair(accountId, key, this.operatorId, this.client);
+    ClientPair pair = new ClientPair(accountId, key, this.operatorId, this.client);
+    // this close wipes everything from this account and sends it back to our guarantor
     assertDoesNotThrow(pair::close,
         "There was a failure closing the pair that was constructed through a re-init wallet");
   }
