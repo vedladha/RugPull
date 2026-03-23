f531c83bdea3e5982f56160a4a7334d29352d05e
JOSEF WOLF <wolf27@wisc.edu>
2026-03-23 16:00:27 -0500

    Fixed sql linting issues

--- database/database_init/004_item_images.sql
+++ database/database_init/004_item_images.sql
@@ -3,23 +3,23 @@ SET time_zone = '+00:00';
 
 START TRANSACTION;
 
-CREATE TABLE ItemImages (
+CREATE TABLE itemimages (
     image_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     user_id INT NOT NULL,
     item_id INT NOT NULL,
     image_url VARCHAR(2048) NOT NULL,
     alt_text TEXT NOT NULL,
-    position INT DEFAULT 0,
+    `position` INT DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_itemimages_items
-        FOREIGN KEY (item_id) REFERENCES items(item_id)
-        ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items (item_id)
+    ON DELETE CASCADE,
 
     CONSTRAINT fk_itemimages_users
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    ON DELETE CASCADE
 );
 
 COMMIT;
