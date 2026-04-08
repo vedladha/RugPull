42b3cd07c5306b760417f328205d56c1d3b5485b
JOSEF WOLF <wolf27@wisc.edu>
2026-04-08 17:18:20 -0500

    Fixed database linting issues

--- database/database_init/006_orders.sql
+++ database/database_init/006_orders.sql
@@ -6,16 +6,18 @@ START TRANSACTION;
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    order_status ENUM('PENDING', 'AWAITING_CONFIRMATION', 'COMPLETED', 'CANCELLED', 'FAILED')
-      DEFAULT 'pending'
-      NOT NULL,
+    order_status ENUM(
+        'PENDING', 'AWAITING_CONFIRMATION', 'COMPLETED', 'CANCELLED', 'FAILED'
+    )
+    DEFAULT 'pending'
+    NOT NULL,
     total_price DECIMAL(30, 8) NOT NULL, -- Total price of all items in crypto
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_orders_users
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-        ON DELETE SET NULL
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
@@ -26,12 +28,12 @@ CREATE TABLE IF NOT EXISTS order_items (
     unit_price DECIMAL(30, 8) NOT NULL, -- Price in crypto
 
     CONSTRAINT fk_orderitems_orders
-        FOREIGN KEY (order_id) REFERENCES orders(order_id)
-        ON DELETE CASCADE,
-    
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
+    ON DELETE CASCADE,
+
     CONSTRAINT fk_orderitems_items
-        FOREIGN KEY(item_id) REFERENCES items(item_id)
-        ON DELETE SET NULL
+    FOREIGN KEY (item_id) REFERENCES items (item_id)
+    ON DELETE SET NULL
 );
 
 COMMIT;
