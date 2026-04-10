ebe9587f0fb232527eaac3a5d040cd0019f0b39a
mholder2 <mholder2@wisc.edu>
2026-04-10 16:40:03 -0500

    fix db linting

--- database/database_init/007_daily_reward.sql
+++ database/database_init/007_daily_reward.sql
@@ -4,6 +4,6 @@ SET time_zone = '+00:00';
 CREATE TABLE IF NOT EXISTS daily_rewards (
     user_id INT,
     claimed_last DATETIME,
-    PRIMARY KEY(user_id)
+    PRIMARY KEY(user_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id)
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
